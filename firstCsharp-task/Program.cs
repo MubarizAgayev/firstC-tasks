dec87bc175e8a6b63a138fae081423273300b0b0
mubarizra@code.edu.az <mubarizra@code.edu.az>
2022-12-08 01:50:47 +0400

    firstC#task

---
+++ firstCsharp-task/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+// task1- Verilmis n ededinin 3-e ve 7-e bolunub-bolunmemesini tapin
+//int n=63;
+//if (n % 3 == 0 && n % 7 == 0)
+//{
+//    Console.WriteLine("n 3-e ve 7-e bolunur");
+//}
+//else
+//    Console.WriteLine("n 3-e ve 7-e bolunmur");
+
+
+
+// task2- n ve m ededleri verilir.eger n ve m ededleri her ikisi cutdurse n ve m ededlerinin cemini hesablayin
+//int n=8, m=16, sum = 0;
+//if (n % 2 == 0 && m % 2 == 0)
+//{
+//    sum = n + m;
+//    Console.WriteLine(sum);
+//}
+
+//else
+//{
+//    Console.WriteLine("duzgun reqem daxil edin");
+//}
+
+
+
+// task3- verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin
+//int n=2, m=9, sum = 0;
+//for (int i = n; i < m; i++) 
+//{
+//    if (i % 2 == 1) 
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum); 
+
+
+
+// task4- verilmis  arrayin icindeki tek ededlerin cemini tapin
+//int[] numbers = new int[7];
+//int sum=0;
+//numbers[0] = 1;
+//numbers[1] = 2;
+//numbers[2] = 3;
+//numbers[3] = 4;
+//numbers[4] = 5;
+//numbers[5] = 6;
+//numbers[6] = 7;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] % 2 == 1) 
+//    {
+//        sum += numbers[i];
+//    }
+//}
+//Console.WriteLine(sum);
+
+
+
+// task5- verilmis arrayin icindeki cut ededlerin sayini tapin
+//int[] numbers= new int[7];
+//int count = 0;
+//numbers[0] = 1;
+//numbers[1] = 2;
+//numbers[2] = 3;
+//numbers[3] = 4;
+//numbers[4] = 5;
+//numbers[5] = 6;
+//numbers[6] = 7;
+//for (int i = 0;i<numbers.Length;i++)
+//{
+//    if (numbers[i] % 2 == 0)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
